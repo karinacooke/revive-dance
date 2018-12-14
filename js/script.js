fb6c732b28cb8f9fabfcc3f9f4c6d67f0d99641f
Karina Cooke <kcooke2012@karinas-air.attlocal.net>
2018-12-14 15:26:35 -0800

    revive in visual studio

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
 const nav = document.querySelector("#navigation");
 const navTop = nav.offsetTop;
+const logo = document.querySelector(".logo");
 
-function stickNavigation (){
+
+function stickyNavigation() {
     if (window.scrollY > navTop){
-        document.querySelector("#navigation").classList.add("nav-inverse");
-        document.querySelector("#navigation").classList.remove("nav-color");
-    }else {
-        document.querySelector("#navigation").classList.remove("nav-inverse");
-        document.querySelector("#navigation").classList.add("nav-color");
+        console.log("moved")
+        document.getElementById("navigation").classList.add("nav-inverse");
+        document.getElementById("navigation").classList.remove("nav-color");
+        // document.querySelector(".list-item").classList.add("list-item-inverse");
+        logo.src = "img/revive-white-logo.jpg";
+    }else{
+        document.getElementById("navigation").classList.remove("nav-inverse");
+        document.getElementById("navigation").classList.add("nav-color");
+        // document.querySelector(".list-item").classList.remove("list-item-inverse");
+        logo.src = "img/revive-black-logo.jpg";
     }
 }
 
-window.addEventListener("click", stickNavigation);
+window.addEventListener("scroll", stickyNavigation);
